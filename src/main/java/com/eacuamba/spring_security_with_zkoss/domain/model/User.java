1e2d239f8b8d9dfecce3f20799c15c2c5cbe1c16
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-14 23:20:28 +0200

    First commit.

---
+++ src/main/java/com/eacuamba/spring_security_with_zkoss/domain/model/User.java
@@ -0,0 +1,64 @@
+package com.eacuamba.spring_security_with_zkoss.domain.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+    @Column(name = "credential_non_expired")
+    private boolean credentialsNonExpired;
+
+    private boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
---
+++ src/main/java/com/eacuamba/spring_security_with_zkoss/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eacuamba.spring_security_with_zkoss.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
