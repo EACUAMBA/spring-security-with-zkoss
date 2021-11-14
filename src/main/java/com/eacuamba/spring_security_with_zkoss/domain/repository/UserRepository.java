459e0459d8f017d2acc7899e3f23f22ec6772653
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-15 00:37:25 +0200

    First commit.

--- src/main/java/com/eacuamba/spring_security_with_zkoss/domain/repository/UserRepository.java
+++ src/main/java/com/eacuamba/spring_security_with_zkoss/domain/repository/UserRepository.java
@@ -1,8 +1,20 @@
 package com.eacuamba.spring_security_with_zkoss.domain.repository;
 
+import com.eacuamba.spring_security_with_zkoss.domain.model.User;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 public class UserRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    public List<User> findAllUsers(){
+        TypedQuery<User> userTypedQuery = this.entityManager.createQuery("select u from User u", User.class);
+        return userTypedQuery.getResultList();
+    }
 }
--- src/main/java/com/eacuamba/spring_security_with_zkoss/domain/service/UserService.java
+++ src/main/java/com/eacuamba/spring_security_with_zkoss/domain/service/UserService.java
@@ -1,7 +1,24 @@
 package com.eacuamba.spring_security_with_zkoss.domain.service;
 
+import com.eacuamba.spring_security_with_zkoss.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return this.userRepository.findAllUsers().stream().findFirst().orElseThrow(()-> new RuntimeException("User not found!"));
+    }
 }
